234028360ab188d3c8b4bbee7b8289e2de8f5a99
krptonox <aryanyadav182007@gmail.com>
2026-07-04 01:22:10 +0530

    feat(password): implement password validation and error handling

---
+++ src/errors/InvalidPasswordError.js
@@ -0,0 +1,9 @@
+class InvalidPasswordError extends Error{
+    constructor(message = "Invalid password provided."){
+        super(message);
+        this.name = "InvalidPasswordError";
+        this.message = message;
+    }
+}
+
+export default InvalidPasswordError;
---
+++ src/password/validatePassword.js
@@ -0,0 +1,18 @@
+import {InvalidPasswordError} from "../errors/InvalidPasswordError.js";
+import { MAX_PASSWORD_LENGTH } from "../constants/password.js";
+
+export function validatePassword(password){
+    if(typeof password !== 'string'){
+        throw new InvalidPasswordError("Password must be a string.");
+    }
+
+    if(password === ""){
+        throw new InvalidPasswordError("Password is required.");
+    }
+
+    if(password.length > MAX_PASSWORD_LENGTH){
+        throw new InvalidPasswordError("Password is too long.");
+    }
+
+    return password;
+}
