cc36a06bc57b0102dc413457b8d0001ce26a0fdc
krptonox <aryanyadav182007@gmail.com>
2026-07-17 17:36:42 +0530

    feat: update package description for clarity and enhance exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // src/index.js
+
 export { hash } from './core/hash.core.js';
 
 export { verifyPassword } from './core/verify.core.js';
 
 export { needsRehash } from './core/needsRehash.core.js';
+
+export { verifyAndRehash } from './core/verifyAndRehash.core.js';
+
+export { checkPasswordBreach } from './core/checkPasswordBreach.core.js';
+
+
+
 
 export { default as InvalidHashError } from './errors/InvalidHashError.js';
 
@@ -11,6 +19,4 @@ export { default as InvalidPasswordError } from './errors/InvalidPasswordError.j
 
 export { default as InvalidOptionsError } from './errors/InvalidOptionsError.js';
 
-export { verifyAndRehash } from './core/verifyAndRehash.core.js';
-
-export { checkPasswordBreach } from './core/checkPasswordBreach.core.js';
+export { default as BreachCheckError } from './errors/BreachCheckError.js';
